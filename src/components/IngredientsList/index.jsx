fde4ad0998284b9e2e4421f84b2521262f176f16
Hsiang <q224399377@gmail.com>
2023-04-11 05:33:53 +0800

    Fix dark mode

--- src/components/IngredientsList/index.jsx
+++ src/components/IngredientsList/index.jsx
@@ -1,22 +1,26 @@
+import { useSelector } from "react-redux";
+import { selectLightMode } from "../../redux/colorSlice";
 import styles from "./ingredientsList.module.css"
 
 const IngredientsList = ({ ingredients, speed = 5000 }) => {
+    const lightMode = useSelector(selectLightMode)
+
     return (
       <div className={styles.inner}>
         <div className={styles.wrapper}>
           <div style={{ "--speed": `${speed}ms` }} className={styles.animation}>
-            {ingredients.map(({ name, image }) => (
-                <img src={image} alt={name} className={styles.item} key={name} />
+            {ingredients.map(({ name, image, imageD }) => (
+                <img src={lightMode ? image : imageD} alt={name} className={styles.item} key={name} />
             ))}
           </div>
           <div style={{ "--speed": `${speed}ms` }} className={styles.animation}>
-            {ingredients.map(({ name, image }) => (
-                <img src={image} alt={name} className={styles.item} key={name} />
+          {ingredients.map(({ name, image, imageD }) => (
+                <img src={lightMode ? image : imageD} alt={name} className={styles.item} key={name} />
             ))}
           </div>
           <div style={{ "--speed": `${speed}ms` }} className={styles.animation}>
-            {ingredients.map(({ name, image }) => (
-                <img src={image} alt={name} className={styles.item} key={name} />
+          {ingredients.map(({ name, image, imageD }) => (
+                <img src={lightMode ? image : imageD} alt={name} className={styles.item} key={name} />
             ))}
           </div>
         </div>
