d8ed88ecebc90f36957a936ef7d5a00431417b1b
蔡相襄 <78093303+hsiang2@users.noreply.github.com>
2023-05-30 02:17:01 +0800

    Edit text align

--- src/components/CheckoutSteps/index.jsx
+++ src/components/CheckoutSteps/index.jsx
@@ -5,8 +5,8 @@ const CheckoutSteps = (props) => {
         token: { colorTextBase },
     } = theme.useToken();
     return (
-        <div className="container checkout-steps" style={{marginTop: '9.5rem', color: colorTextBase}}>
-            <div className={props.step1 ? 'active' : ''}>Sign-In</div>
+        <div className="container checkout-steps" style={{marginTop: '9.5rem', color: colorTextBase, textAlign: 'center'}}>
+            <div className={props.step1 ? 'active' : ''} >Sign-In</div>
             <div className={props.step2 ? 'active' : ''}>Shipping</div>
             <div className={props.step3 ? 'active' : ''}>Payment</div>
             <div className={props.step4 ? 'active' : ''}>Place Order</div>
